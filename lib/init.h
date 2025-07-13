e84ae42b489b7d430710f203a06bf125a18170b4
ohpxho <lemuel.k.costuna.so@gmail.com>
2025-07-13 23:45:53 +0800

    added definition for reading from a file or prompt

---
+++ lib/init.h
@@ -0,0 +1,9 @@
+#ifndef INIT_H
+#define INIT_H
+
+#include <stdio.h>
+
+void run_file(const char* path);
+void run_prompt();
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../lib/init.h"
+
+int main() {
+    return 0;
+}
+
+void run_file(const char* path) {
+
+}
+
+void run_prompt() {
+
+}
